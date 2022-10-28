82f79e5e49110e3c2f4761dbe1c439d1a577a96b
marcel-noehre <marcelnoehre@web.de>
2022-10-29 01:29:05 +0200

    2017-02 solution

---
+++ 2017/02/solution.js
@@ -0,0 +1,31 @@
+"use strict";
+exports.__esModule = true;
+var fs_1 = require("fs");
+var file = (0, fs_1.readFileSync)('puzzle.txt', 'utf-8');
+var arr = file.toString().trim().split('\r\n').map(function (row) { return row.split('\t').map(function (number) { return parseInt(number, 10); }); });
+console.log(part_1(arr));
+console.log(part_2(arr));
+function part_1(spreadsheet) {
+    var checksum = 0;
+    for (var i = 0; i < spreadsheet.length; i++) {
+        checksum += Math.max.apply(Math, spreadsheet[i]) - Math.min.apply(Math, spreadsheet[i]);
+    }
+    return checksum;
+}
+function part_2(spreadsheet) {
+    var checksum = 0;
+    for (var i = 0; i < spreadsheet.length; i++) {
+        for (var j = 0; j < spreadsheet[i].length - 1; j++) {
+            for (var x = j + 1; x < spreadsheet[i].length; x++) {
+                var max = Math.max(spreadsheet[i][j], spreadsheet[i][x]);
+                var min = Math.min(spreadsheet[i][j], spreadsheet[i][x]);
+                if (max % min === 0) {
+                    checksum += max / min;
+                    j = spreadsheet[i].length;
+                    x = spreadsheet[i].length;
+                }
+            }
+        }
+    }
+    return checksum;
+}
---
+++ 2017/02/solution.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from 'fs';
+
+const file:any = readFileSync('puzzle.txt', 'utf-8');
+const arr: number[][] = file.toString().trim().split('\r\n').map((row) => row.split('\t').map((number) => parseInt(number, 10)));
+console.log(part_1(arr));
+console.log(part_2(arr));
+
+function part_1(spreadsheet: number[][]): number {
+    let checksum: number = 0;
+    for(let i = 0; i < spreadsheet.length; i++) {
+        checksum += Math.max(...spreadsheet[i]) - Math.min(...spreadsheet[i]);
+    }
+    return checksum;
+}
+
+function part_2(spreadsheet: number[][]): number {
+    let checksum: number = 0;
+    for(let i = 0; i < spreadsheet.length; i++) {
+        for(let j = 0; j < spreadsheet[i].length-1; j++) {
+            for(let x = j+1; x < spreadsheet[i].length; x++) {
+                let max: number = Math.max(spreadsheet[i][j], spreadsheet[i][x]);
+                let min: number = Math.min(spreadsheet[i][j], spreadsheet[i][x]);
+                if(max % min === 0) {
+                    checksum += max/min;
+                    j = spreadsheet[i].length;
+                    x = spreadsheet[i].length;
+                }
+            }
+        }
+    }
+    return checksum;
+}
