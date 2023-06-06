80e0f072853058626d507a86fd24a0e0e2ff4e57
marcel-noehre <marcelnoehre@web.de>
2023-06-06 14:34:00 +0200

    cleanup 2022/01 java

--- year2022/day01/java/Solution.java
+++ year2022/day01/java/Solution.java
@@ -4,45 +4,38 @@ import java.nio.file.*;
 import java.util.*;
 
 public class Solution {
+    private static Map<Integer, Integer> ELFS = new HashMap<Integer, Integer>();
+    
     public static void main(String[] args) { 
         try {
-            List<String> puzzle = Files.readAllLines(Paths.get("../puzzle.txt"));
-            System.out.println(part1(puzzle));
-            System.out.println(part2(puzzle));
+            setElfs(Files.readAllLines(Paths.get("../puzzle.txt")));
+            System.out.println(part1());
+            System.out.println(part2());
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
     
-	private static Integer part1(List<String> input) {
-    	Map<Integer, Integer> elfs = new HashMap<Integer, Integer>();
-    	int elf = 1;
-    	int sum = 0;
-    	for(String calory : input) {
-    		if(calory.isBlank()) {
-    			elfs.put(elf, sum);
-                sum = 0;
-                elf++;
-    		} else {
-    			sum += Integer.parseInt(calory);
-    		}
-    	}
-        return Collections.max(elfs.values());
+	private static Integer part1() {
+        return Collections.max(ELFS.values());
     }
     
-    private static Integer part2(List<String> input) {
-    	Map<Integer, Integer> elfs = new HashMap<Integer, Integer>();
-    	int elf = 1;
-    	int sum = 0;
-    	for(String calory : input) {
-    		if(calory.isBlank()) {
-    			elfs.put(elf, sum);
+    private static Integer part2() {
+        return ELFS.values().stream().sorted(Collections.reverseOrder())
+                .limit(3).mapToInt(Integer::intValue).sum();
+    }
+    
+    private static void setElfs(List<String> input) {
+        int elf = 1;
+        int sum = 0;
+        for(String calory : input) {
+            if(calory.isBlank()) {
+                ELFS.put(elf, sum);
                 sum = 0;
                 elf++;
-    		} else {
-    			sum += Integer.parseInt(calory);
-    		}
-    	}
-        return elfs.values().stream().sorted(Collections.reverseOrder()).limit(3).mapToInt(Integer::intValue).sum();
+            } else {
+                sum += Integer.parseInt(calory);
+            }
+        }
     }
 }
