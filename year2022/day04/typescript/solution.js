2efc9d63199f3c281a6df7430dd9dfffd4eb1e01
marcel-noehre <marcelnoehre@web.de>
2023-06-08 23:33:21 +0200

    cleanup 2022/04

--- year2022/day04/typescript/solution.js
+++ year2022/day04/typescript/solution.js
@@ -3,35 +3,27 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().trim().split('\r\n').map(function (group) { return group.split(',').map(function (tupel) { return tupel.split('-').map(function (num) { return parseInt(num, 10); }); }); });
-console.log(part_1(arr));
-console.log(part_2(arr));
-function part_1(list) {
-    var sum = 0;
-    list.forEach(function (group) {
-        if (group[0][0] <= group[1][0] && group[0][1] >= group[1][1]) {
-            sum++;
-        }
-        else if (group[1][0] <= group[0][0] && group[1][1] >= group[0][1]) {
-            sum++;
-        }
-    });
-    return sum;
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return arr.filter(function (_a) {
+        var range1 = _a[0], range2 = _a[1];
+        return overlaps(range1, range2);
+    }).length;
 }
-function part_2(list) {
-    var sum = 0;
-    list.forEach(function (group) {
-        if (group[0][0] >= group[1][0] && group[0][0] <= group[1][1]) {
-            sum++;
-        }
-        else if (group[1][0] >= group[0][0] && group[1][0] <= group[0][1]) {
-            sum++;
-        }
-        else if (group[0][1] <= group[1][1] && group[0][1] >= group[1][0]) {
-            sum++;
-        }
-        else if (group[1][1] <= group[0][1] && group[1][1] >= group[0][0]) {
-            sum++;
-        }
-    });
-    return sum;
+function part_2() {
+    return arr.filter(function (_a) {
+        var range1 = _a[0], range2 = _a[1];
+        return contains(range1, range2);
+    }).length;
+}
+function overlaps(range1, range2) {
+    return ((range1[0] <= range2[0] && range1[1] >= range2[1]) ||
+        (range2[0] <= range1[0] && range2[1] >= range1[1]));
+}
+function contains(range1, range2) {
+    return ((range1[0] >= range2[0] && range1[0] <= range2[1]) ||
+        (range2[0] >= range1[0] && range2[0] <= range1[1]) ||
+        (range1[1] <= range2[1] && range1[1] >= range2[0]) ||
+        (range2[1] <= range1[1] && range2[1] >= range1[0]));
 }
--- year2022/day04/typescript/solution.ts
+++ year2022/day04/typescript/solution.ts
@@ -2,33 +2,29 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr: number[][][] = file.toString().trim().split('\r\n').map((group) => group.split(',').map(tupel => tupel.split('-').map(num => parseInt(num, 10))));
-console.log(part_1(arr));
-console.log(part_2(arr));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(list: number[][][]):number {
-    let sum: number = 0;
-    list.forEach(group => {
-        if(group[0][0] <= group[1][0] && group[0][1] >= group[1][1]) {
-            sum++;
-        } else if(group[1][0] <= group[0][0] && group[1][1] >= group[0][1]) {
-            sum++;
-        }
-    });
-    return sum;
-}
-
-function part_2(list: number[][][]):number {
-    let sum: number = 0;
-    list.forEach(group => {
-        if(group[0][0] >= group[1][0] && group[0][0] <= group[1][1]) {
-            sum++;
-        } else if(group[1][0] >= group[0][0] && group[1][0] <= group[0][1]) {
-            sum++;
-        } else if(group[0][1] <= group[1][1] && group[0][1] >= group[1][0]) {
-            sum++;
-        } else if(group[1][1] <= group[0][1] && group[1][1] >= group[0][0]) {
-            sum++;
-        } 
-    });
-    return sum;
-}
+function part_1() {
+    return arr.filter(([range1, range2]) => overlaps(range1, range2)).length;
+  }
+  
+  function part_2() {
+    return arr.filter(([range1, range2]) => contains(range1, range2)).length;
+  }
+  
+  function overlaps(range1, range2) {
+    return (
+      (range1[0] <= range2[0] && range1[1] >= range2[1]) ||
+      (range2[0] <= range1[0] && range2[1] >= range1[1])
+    );
+  }
+  
+  function contains(range1, range2) {
+    return (
+      (range1[0] >= range2[0] && range1[0] <= range2[1]) ||
+      (range2[0] >= range1[0] && range2[0] <= range1[1]) ||
+      (range1[1] <= range2[1] && range1[1] >= range2[0]) ||
+      (range2[1] <= range1[1] && range2[1] >= range1[0])
+    );
+  }
