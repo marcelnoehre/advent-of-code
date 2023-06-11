c1838f9e72ac0007326cf2b62bd646c0dbc23b63
marcel-noehre <marcelnoehre@web.de>
2023-06-11 20:20:05 +0200

    cleanup 2022/07

--- year2022/day07/typescript/solution.js
+++ year2022/day07/typescript/solution.js
@@ -1,140 +1,48 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().split('\r\n');
-var global_sum_1 = 0;
-var global_sum_2 = 0;
-var global_list_2 = [];
-console.log(part_1(arr));
-console.log(part_2(arr));
-function part_1(commands) {
-    var system = [];
-    var path = [];
-    commands.forEach(function (command) {
-        if (command.startsWith('$')) {
-            if (command.startsWith('$ cd')) {
-                var routeChange = command.split(' ');
-                if (routeChange[2] == '/') {
-                    path = [];
-                }
-                else if (routeChange[2] == '..') {
-                    path.pop();
-                }
-                else {
-                    path.push(routeChange[2]);
-                }
-            }
-        }
-        else if (command.startsWith('dir')) {
-            var tmp_1 = system;
-            path.forEach(function (route) {
-                for (var i = 0; i < tmp_1.length; i++) {
-                    if (tmp_1[i][0] == 'folder_' + route) {
-                        var max = tmp_1.length;
-                        tmp_1 = tmp_1[i];
-                        i = max;
-                    }
-                }
-            });
-            tmp_1.push(['folder_' + command.split(' ')[1]]);
-        }
-        else {
-            var tmp_2 = system;
-            path.forEach(function (route) {
-                for (var i = 0; i < tmp_2.length; i++) {
-                    if (tmp_2[i][0] == 'folder_' + route) {
-                        var max = tmp_2.length;
-                        tmp_2 = tmp_2[i];
-                        i = max;
-                    }
-                }
-            });
-            var file_1 = command.split(' ');
-            tmp_2.push('file_' + file_1[1] + '_' + file_1[0]);
-        }
-    });
-    global_sum_2 = recursive_1(system);
-    return global_sum_1;
+var filesystem = {};
+setFileSystem();
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return Object.values(filesystem)
+        .filter(function (size) { return size <= 100000; })
+        .reduce(function (acc, size) { return acc + size; }, 0);
 }
-function part_2(commands) {
-    var system = [];
-    var path = [];
-    commands.forEach(function (command) {
-        if (command.startsWith('$')) {
-            if (command.startsWith('$ cd')) {
-                var routeChange = command.split(' ');
-                if (routeChange[2] == '/') {
-                    path = [];
-                }
-                else if (routeChange[2] == '..') {
-                    path.pop();
-                }
-                else {
-                    path.push(routeChange[2]);
-                }
-            }
-        }
-        else if (command.startsWith('dir')) {
-            var tmp_3 = system;
-            path.forEach(function (route) {
-                for (var i = 0; i < tmp_3.length; i++) {
-                    if (tmp_3[i][0] == 'folder_' + route) {
-                        var max = tmp_3.length;
-                        tmp_3 = tmp_3[i];
-                        i = max;
-                    }
-                }
-            });
-            tmp_3.push(['folder_' + command.split(' ')[1]]);
-        }
-        else {
-            var tmp_4 = system;
-            path.forEach(function (route) {
-                for (var i = 0; i < tmp_4.length; i++) {
-                    if (tmp_4[i][0] == 'folder_' + route) {
-                        var max = tmp_4.length;
-                        tmp_4 = tmp_4[i];
-                        i = max;
-                    }
-                }
-            });
-            var file_2 = command.split(' ');
-            tmp_4.push('file_' + file_2[1] + '_' + file_2[0]);
-        }
-    });
-    recursive_2(system);
-    return global_list_2.sort(function (n1, n2) { return n1 - n2; })[0];
+function part_2() {
+    return Math.min.apply(Math, Object.values(filesystem)
+        .filter(function (size) { return size >= (30000000 - (70000000 - filesystem['/'])); }));
 }
-function recursive_1(array) {
-    var sum = 0;
-    array.forEach(function (object) {
-        if (typeof (object) == 'string') {
-            if (object.startsWith('file')) {
-                sum += parseInt(object.split('_')[2], 10);
+function setFileSystem() {
+    var visited = new Set();
+    var current = [];
+    arr.forEach(function (command) {
+        if (command.match(/^\$ cd (.+)$/)) {
+            current = command.split(' ')[2] === '..' ? current.slice(0, -1) : __spreadArray(__spreadArray([], current, true), [command.split(' ')[2]], false);
+        }
+        else if (command.match(/^(\d+) (.+)$/)) {
+            var _a = [parseInt(command.split(' ')[0]), command.split(' ')[1]], size = _a[0], name_1 = _a[1];
+            if (!visited.hasOwnProperty(current.toString() + '_' + name_1)) {
+                visited.add(current.toString() + '_' + name_1);
+                var dir = current;
+                while (dir[0]) {
+                    filesystem[dir.toString()] = (filesystem[dir.toString()] || 0) + size;
+                    dir = dir.slice(0, -1);
+                }
             }
         }
-        else {
-            sum += recursive_1(object);
-        }
     });
-    global_sum_1 += sum <= 100000 ? sum : 0;
-    return sum;
-}
-function recursive_2(array) {
-    var sum = 0;
-    array.forEach(function (object) {
-        if (typeof (object) == 'string') {
-            if (object.startsWith('file')) {
-                sum += parseInt(object.split('_')[2], 10);
-            }
-        }
-        else {
-            sum += recursive_2(object);
-        }
-    });
-    if (sum + 70000000 - global_sum_2 >= 30000000) {
-        global_list_2.push(sum);
-    }
-    return sum;
+    return filesystem;
 }
