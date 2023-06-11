1ced2a873db0b63e14e68847c0977845dfedb7c8
marcel-noehre <marcelnoehre@web.de>
2023-06-12 01:38:58 +0200

    cleanup 2020/01

--- year2020/day01/typescript/solution.js
+++ year2020/day01/typescript/solution.js
@@ -3,28 +3,19 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().trim().split('\n').map(function (num) { return parseInt(num, 10); });
-console.log(part_1(arr));
-console.log(part_2(arr));
-function part_1(numbers) {
-    for (var i = 0; i < numbers.length; i++) {
-        for (var y = 0; y < numbers.length; y++) {
-            if (i != y && numbers[i] + numbers[y] == 2020) {
-                return numbers[i] * numbers[y];
-            }
-        }
-    }
-    return null;
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return arr.reduce(function (result, num, index, arr) {
+        return arr.includes(2020 - num, index + 1) ? num * (2020 - num) : result;
+    }, 0);
 }
-function part_2(numbers) {
-    for (var i = 0; i < numbers.length; i++) {
-        for (var y = 0; y < numbers.length; y++) {
-            for (var x = 0; x < numbers.length; x++) {
-                if (i != y && i != x && y != x) {
-                    if (numbers[i] + numbers[y] + numbers[x] == 2020) {
-                        return numbers[i] * numbers[y] * numbers[x];
-                    }
-                }
-            }
-        }
-    }
+function part_2() {
+    return arr.reduce(function (result, num, index, arr) {
+        return arr.slice(index + 1).reduce(function (innerResult, innerNum) {
+            return arr.slice(index + 2).reduce(function (finalResult, finalNum) {
+                return num + innerNum + finalNum === 2020 ? num * innerNum * finalNum : finalResult;
+            }, innerResult);
+        }, result);
+    }, 0);
 }
--- year2020/day01/typescript/solution.ts
+++ year2020/day01/typescript/solution.ts
@@ -2,30 +2,17 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr:number[] = file.toString().trim().split('\n').map((num) => parseInt(num, 10));
-console.log(part_1(arr));
-console.log(part_2(arr));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(numbers: number[]): number {
-    for(let i = 0; i < numbers.length; i++) {
-        for(let y = 0; y < numbers.length; y++) {
-            if(i != y && numbers[i] + numbers[y] == 2020) {
-                return numbers[i] * numbers[y];
-            }
-        }
-    } 
-    return null;
+function part_1(): number {
+    return arr.reduce((result, num, index, arr) => 
+            arr.includes(2020 - num, index + 1) ? num * (2020 - num) : result, 0);
 }
 
-function part_2(numbers: number[]): number {
-    for(let i = 0; i < numbers.length; i++) {
-        for(let y = 0; y < numbers.length; y++) {
-            for(let x = 0; x < numbers.length; x++) {
-                if(i != y && i != x && y != x) {
-                    if(numbers[i] + numbers[y] + numbers[x] == 2020) {
-                        return numbers[i] * numbers[y] * numbers[x];
-                    }
-                }
-            }
-        }
-    } 
-}
+function part_2(): number {
+    return arr.reduce((result, num, index, arr) =>
+            arr.slice(index + 1).reduce((innerResult, innerNum) =>
+            arr.slice(index + 2).reduce((finalResult, finalNum) =>
+            num + innerNum + finalNum === 2020 ? num * innerNum * finalNum : finalResult, innerResult), result), 0);
+  }
