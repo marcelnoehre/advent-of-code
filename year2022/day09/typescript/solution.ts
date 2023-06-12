3ab0ea5351e549b2a614f0a79bfc655aa4025aea
marcel-noehre <marcelnoehre@web.de>
2023-06-12 16:23:00 +0200

    cleanup 2022/09

--- year2022/day09/typescript/solution.ts
+++ year2022/day09/typescript/solution.ts
@@ -2,75 +2,66 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr: string[][] = file.toString().trim().split('\r\n').map((pair) => pair.split(' '));
+const directions = {
+    U: [0, 1],
+    D: [0, -1],
+    L: [-1, 0],
+    R: [1, 0]
+}
 console.log(part_1(arr));
 console.log(part_2(arr));
 
 function part_1(list: string[][]):number {
-    let tailPositions: Set<number[]> = new Set([[0,0]]);
+    const tailPositions: number[][] = [[0,0]];
     let head: number[] = [0,0];
     let tail: number[] = [0,0];
     list.forEach(pair => {
         for(let i = 0; i < parseInt(pair[1]); i++) {
-            if(pair[0] == 'U') {
-                head[1]++;
-            } else if(pair[0] == 'D') {
-                head[1]--;
-            } else if(pair[0] == 'L') {
-                head[0]--;
-            } else if(pair[0] == 'R') {
-                head[0]++;
-            }
-            if(Math.max(Math.abs(tail[0]-head[0]),Math.abs(tail[1]-head[1])) > 1) {
-                tail = [Math.abs(head[0]-tail[0]) === 2 ? (tail[0]+(head[0]-tail[0])/2) : (tail[0]+(head[0]-tail[0])), Math.abs(head[1]-tail[1]) === 2 ? (tail[1]+(head[1]-tail[1])/2) : (tail[1]+(head[1]-tail[1]))]; 
-                let check:boolean = true;
-                tailPositions.forEach(pos => {
-                    if(pos[0] === tail[0] && pos[1] === tail[1]) {
-                        check = false;
-                    }
-                });            
-                if(check) {
-                    tailPositions.add(tail);
+            head[0] += directions[pair[0]][0];
+            head[1] += directions[pair[0]][1];
+            if(Math.max(Math.abs(tail[0] - head[0]), Math.abs(tail[1] - head[1])) > 1) {
+                tail = [Math.abs(head[0] - tail[0]) === 2
+                        ? (tail[0] + (head[0] - tail[0]) / 2)
+                        : (tail[0] + (head[0] - tail[0])),
+                        Math.abs(head[1] - tail[1]) === 2
+                        ? (tail[1] + (head[1] - tail[1]) / 2)
+                        : (tail[1] + (head[1] - tail[1]))]; 
+                if(checkTailPosition(tailPositions, tail[0], tail[1])) {
+                    tailPositions.push(tail);
                 }
             }
         }
     });
-    return tailPositions.size;
+    return tailPositions.length;
 }
 
 function part_2(list: string[][]):number {
-    let tailPositions: Set<number[]> = new Set([[0,0]]);
+    let tailPositions: number[][] = [[0,0]];
     let head: number[] = [0,0];
-    let tail: number[][] = [];
-    for(let i = 0; i < 9; i++) {
-        tail.push([0,0]);
-    }
+    let tail: number[][] = Array.from({ length: 9 }, () => [0, 0]);
     list.forEach(pair => {
         for(let i = 0; i < parseInt(pair[1]); i++) {
-            if(pair[0] == 'U') {
-                head[1]++;
-            } else if(pair[0] == 'D') {
-                head[1]--;
-            } else if(pair[0] == 'L') {
-                head[0]--;
-            } else if(pair[0] == 'R') {
-                head[0]++;
-            }
+            head[0] += directions[pair[0]][0];
+            head[1] += directions[pair[0]][1];
             for(let j = 0; j < 9; j++) {
-                let reference: number[] = j === 0 ? head : tail[j-1];
-                if(Math.max(Math.abs(tail[j][0]-reference[0]),Math.abs(tail[j][1]-reference[1])) > 1) {
-                    tail[j] = [Math.abs(reference[0]-tail[j][0]) === 2 ? (tail[j][0]+(reference[0]-tail[j][0])/2) : (tail[j][0]+(reference[0]-tail[j][0])), Math.abs(reference[1]-tail[j][1]) === 2 ? (tail[j][1]+(reference[1]-tail[j][1])/2) : (tail[j][1]+(reference[1]-tail[j][1]))]; 
+                let reference: number[] = j === 0 ? head : tail[j - 1];
+                if(Math.max(Math.abs(tail[j][0] - reference[0]), Math.abs(tail[j][1] - reference[1])) > 1) {
+                    tail[j] = [Math.abs(reference[0] - tail[j][0]) === 2
+                            ? (tail[j][0] + (reference[0] - tail[j][0]) / 2)
+                            : (tail[j][0] + (reference[0] - tail[j][0])), 
+                            Math.abs(reference[1] - tail[j][1]) === 2
+                            ? (tail[j][1] + (reference[1] - tail[j][1]) / 2)
+                            : (tail[j][1] + (reference[1] - tail[j][1]))]; 
                 }
             }
-            let check:boolean = true;
-            tailPositions.forEach(pos => {
-                if(pos[0] === tail[8][0] && pos[1] === tail[8][1]) {
-                    check = false;
-                }
-            });            
-            if(check) {
-                tailPositions.add(tail[8]);
+            if(checkTailPosition(tailPositions, tail[8][0], tail[8][1])) {
+                tailPositions.push(tail[8]);
             }
         }
     });
-    return tailPositions.size;
+    return tailPositions.length;
+}
+
+function checkTailPosition(tailPositions: number[][], start: number, end: number): boolean {
+    return !tailPositions.some(pos => pos[0] === start && pos[1] === end);
 }
