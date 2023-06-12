2962f5e87eb48aa30bd815275d3cbbcf7b6480a5
marcel-noehre <marcelnoehre@web.de>
2023-06-12 11:58:04 +0200

    cleanup 2019/01

--- year2019/day01/typescript/solution.js
+++ year2019/day01/typescript/solution.js
@@ -3,23 +3,19 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().trim().split('\n').map(function (num) { return parseInt(num, 10); });
-console.log(part_1(arr));
-console.log(part_2(arr));
-function part_1(numbers) {
-    var sum = 0;
-    for (var i = 0; i < numbers.length; i++) {
-        sum += (Math.floor(numbers[i] / 3) - 2);
-    }
-    return sum;
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return arr.reduce(function (acc, mass) { return acc + Math.floor(mass / 3) - 2; }, 0);
 }
-function part_2(numbers) {
+function part_2() {
     var sum = 0;
-    for (var i = 0; i < numbers.length; i++) {
-        var fuel = (Math.floor(numbers[i] / 3) - 2);
-        do {
+    arr.forEach(function (mass) {
+        var fuel = (Math.floor(mass / 3) - 2);
+        while (fuel > 0) {
             sum += fuel;
             fuel = (Math.floor(fuel / 3) - 2);
-        } while (fuel > 0);
-    }
+        }
+    });
     return sum;
 }
--- year2019/day01/typescript/solution.ts
+++ year2019/day01/typescript/solution.ts
@@ -2,25 +2,21 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr:number[] = file.toString().trim().split('\n').map((num) => parseInt(num, 10));
-console.log(part_1(arr));
-console.log(part_2(arr));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(numbers: number[]): number {
-    let sum: number = 0;
-    for(let i = 0; i < numbers.length; i++) {
-        sum += (Math.floor(numbers[i]/3)-2);
-    }
-    return sum;
+function part_1(): number {
+    return arr.reduce((acc, mass) => acc + Math.floor(mass / 3) - 2, 0);
 }
 
-function part_2(numbers: number[]): number {
-    let sum: number = 0;
-    for(let i = 0; i < numbers.length; i++) {
-        let fuel = (Math.floor(numbers[i]/3)-2); 
-        do {
+function part_2(): number {
+    let sum = 0;
+    arr.forEach((mass) => {
+        let fuel = (Math.floor(mass / 3) - 2);
+        while (fuel > 0) {
             sum += fuel;
-            fuel = (Math.floor(fuel/3)-2);
-        } while(fuel > 0);
-    }
+            fuel = (Math.floor(fuel / 3) - 2)
+        }
+    });
     return sum;
 }
