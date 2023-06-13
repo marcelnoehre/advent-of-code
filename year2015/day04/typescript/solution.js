a4c7fa49287eff6cc71a518c71e73b1676f42afc
marcel-noehre <marcelnoehre@web.de>
2023-06-13 20:37:41 +0200

    cleanup 2015/04

--- year2015/day04/typescript/solution.js
+++ year2015/day04/typescript/solution.js
@@ -3,28 +3,19 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var md5_1 = require("ts-md5/dist/md5");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
-var row = file.toString().trim();
-console.log(part_1(row));
-console.log(part_2(row));
-function part_1(key) {
-    var counter = 0;
-    do {
-        if (md5_1.Md5.hashStr(key + counter).substring(0, 5) == '00000') {
-            return counter;
-        }
-        else {
-            counter++;
-        }
-    } while (true);
+var key = file.toString().trim();
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return mine(5);
 }
-function part_2(key) {
+function part_2() {
+    return mine(6);
+}
+function mine(digits) {
     var counter = 0;
-    do {
-        if (md5_1.Md5.hashStr(key + counter).substring(0, 6) == '000000') {
-            return counter;
-        }
-        else {
-            counter++;
-        }
-    } while (true);
+    while (!(md5_1.Md5.hashStr(key + counter).substring(0, digits) === '0'.repeat(digits))) {
+        counter++;
+    }
+    return counter;
 }
--- year2015/day04/typescript/solution.ts
+++ year2015/day04/typescript/solution.ts
@@ -1,29 +1,23 @@
 import { readFileSync } from 'fs';
 import { Md5 } from 'ts-md5/dist/md5';
 
-const file:any = readFileSync('../puzzle.txt', 'utf-8');
-const row:string = file.toString().trim();
-console.log(part_1(row));
-console.log(part_2(row));
+const file: any = readFileSync('../puzzle.txt', 'utf-8');
+const key: string = file.toString().trim();
+console.log(part_1());
+console.log(part_2());
 
-function part_1(key: string): number {
-    let counter: number = 0;
-    do {
-        if(Md5.hashStr(key + counter).substring(0,5) == '00000') {
-            return counter;
-        } else {
-            counter++;
-        }
-    } while(true);
+function part_1(): number {
+    return mine(5);
 }
 
-function part_2(key: string): number {
+function part_2(): number {
+    return mine(6); 
+}
+
+function mine(digits: number): number {
     let counter: number = 0;
-    do {
-        if(Md5.hashStr(key + counter).substring(0,6) == '000000') {
-            return counter;
-        } else {
-            counter++;
-        }
-    } while(true);
+    while (!(Md5.hashStr(key + counter).substring(0, digits) === '0'.repeat(digits))) {
+        counter++;
+    }
+    return counter;
 }
