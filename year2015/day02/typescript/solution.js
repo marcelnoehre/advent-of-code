489d061c5ab1eeb7a475069a0ce3bdcf0a2817de
marcel-noehre <marcelnoehre@web.de>
2023-06-13 18:34:44 +0200

    cleanup 2015/02

--- year2015/day02/typescript/solution.js
+++ year2015/day02/typescript/solution.js
@@ -2,26 +2,18 @@
 exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
-var presents = file.toString().trim().split('\n').map(function (present) { return present.split('x').map(function (value) { return parseInt(value, 10); }); });
-console.log(part_1(presents));
-console.log(part_2(presents));
-function part_1(presents) {
-    var sum = 0;
-    for (var i = 0; i < presents.length; i++) {
-        var smallest = presents[i][2] * presents[i][0];
-        for (var j = 0; j < 2; j++) {
-            smallest = presents[i][j] * presents[i][j + 1] < smallest ? presents[i][j] * presents[i][j + 1] : smallest;
-        }
-        sum += 2 * presents[i][0] * presents[i][1] + 2 * presents[i][1] * presents[i][2] + 2 * presents[i][2] * presents[i][0] + smallest;
-    }
-    return sum;
+var arr = file.toString().trim().split('\n').map(function (present) { return present.split('x').map(function (value) { return parseInt(value, 10); }); });
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return arr.map(function (box) { return box.sort(function (a, b) { return a - b; }); }).reduce(function (total, _a) {
+        var l = _a[0], w = _a[1], h = _a[2];
+        return total + 2 * (l * w + l * h + w * h) + l * w;
+    }, 0);
 }
-function part_2(presents) {
-    var sum = 0;
-    for (var i = 0; i < presents.length; i++) {
-        var sides = [presents[i][0] <= presents[i][1] ? presents[i][0] : presents[i][1], presents[i][0] <= presents[i][1] ? presents[i][1] : presents[i][0]];
-        sides[1] = presents[i][2] < sides[1] ? presents[i][2] : sides[1];
-        sum += presents[i][0] * presents[i][1] * presents[i][2] + (sides[0] + sides[1]) * 2;
-    }
-    return sum;
+function part_2() {
+    return arr.reduce(function (total, _a) {
+        var l = _a[0], w = _a[1], h = _a[2];
+        return total + (2 * (l + w) + l * w * h);
+    }, 0);
 }
--- year2015/day02/typescript/solution.ts
+++ year2015/day02/typescript/solution.ts
@@ -1,28 +1,14 @@
 import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
-const presents:number[][] = file.toString().trim().split('\n').map((present) => present.split('x').map((value) => parseInt(value, 10)));
-console.log(part_1(presents));
-console.log(part_2(presents));
+const arr: number[][] = file.toString().trim().split('\n').map((present) => present.split('x').map((value) => parseInt(value, 10)));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(presents: number[][]): number {
-    let sum: number = 0;
-    for(let i = 0; i < presents.length; i++) {
-        let smallest: number = presents[i][2]*presents[i][0];
-        for(let j = 0; j < 2; j++) {
-            smallest = presents[i][j]*presents[i][j+1] < smallest? presents[i][j]*presents[i][j+1] : smallest;
-        }
-        sum += 2*presents[i][0]*presents[i][1] + 2*presents[i][1]*presents[i][2] + 2*presents[i][2]*presents[i][0] + smallest;
-    }
-    return sum;
+function part_1(): number {
+    return arr.map((box) => box.sort((a, b) => a - b)).reduce((total, [l, w, h]) => total + 2 * (l * w + l * h + w * h) + l * w, 0);
 }
 
-function part_2(presents: number[][]): number {
-    let sum: number = 0;
-    for(let i = 0; i < presents.length; i++) {
-        let sides: number[] = [presents[i][0] <= presents[i][1]? presents[i][0] : presents[i][1], presents[i][0] <= presents[i][1]? presents[i][1] : presents[i][0]]
-        sides[1] = presents[i][2] < sides[1]? presents[i][2] : sides[1]
-        sum += presents[i][0]*presents[i][1]*presents[i][2] + (sides[0]+sides[1])*2;
-    }
-    return sum;
+function part_2(): number {
+    return arr.reduce((total, [l, w, h]) => total + (2 * (l + w) + l * w * h), 0);
 }
