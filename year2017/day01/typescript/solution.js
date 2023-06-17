891389f7188fbdf3b6547ec25c7c99be77787f2a
marcel-noehre <marcelnoehre@web.de>
2023-06-17 10:27:03 +0200

    cleanup 2017/01

--- year2017/day01/typescript/solution.js
+++ year2017/day01/typescript/solution.js
@@ -3,34 +3,11 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().trim().split('').map(function (num) { return parseInt(num, 10); });
-console.log(part_1(arr));
-console.log(part_2(arr));
-function part_1(numbers) {
-    var sum = 0;
-    for (var i = 1; i < numbers.length; i++) {
-        if (numbers[i] == numbers[i - 1]) {
-            sum += numbers[i];
-        }
-    }
-    if (numbers[numbers.length - 1] == numbers[0]) {
-        sum += numbers[0];
-    }
-    return sum;
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return arr.filter(function (num, index) { return num === arr[(index + 1) % arr.length]; }).reduce(function (acc, val) { return acc + val; }, 0);
 }
-function part_2(numbers) {
-    var sum = 0;
-    var forward = numbers.length / 2;
-    for (var i = 0; i < numbers.length; i++) {
-        if (i + forward >= numbers.length - 1) {
-            if (numbers[i] == numbers[forward - (numbers.length - i)]) {
-                sum += numbers[i];
-            }
-        }
-        else {
-            if (numbers[i] == numbers[i + forward]) {
-                sum += numbers[i];
-            }
-        }
-    }
-    return sum;
+function part_2() {
+    return arr.reduce(function (acc, num, i) { return num === arr[(i + arr.length / 2) % arr.length] ? acc + num : acc; }, 0);
 }
--- year2017/day01/typescript/solution.ts
+++ year2017/day01/typescript/solution.ts
@@ -2,35 +2,14 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr:number[] = file.toString().trim().split('').map((num) => parseInt(num, 10));
-console.log(part_1(arr));
-console.log(part_2(arr));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(numbers: number[]): number {
-    let sum:number = 0;
-    for(let i = 1; i < numbers.length; i++) {
-        if(numbers[i] == numbers[i-1]) {
-            sum += numbers[i];
-        }
-    }
-    if(numbers[numbers.length-1] == numbers[0]) {
-        sum += numbers[0];
-    }
-    return sum;
+function part_1(): number {
+    return arr.filter((num, index) => num === arr[(index + 1) % arr.length]).reduce((acc, val) => acc + val, 0);
+
 }
 
-function part_2(numbers: number[]): number {
-    let sum:number = 0;
-    let forward: number = numbers.length/2;
-    for(let i = 0; i < numbers.length; i++) {
-        if(i+forward >= numbers.length-1) {
-            if(numbers[i] == numbers[forward-(numbers.length-i)]) {
-                sum += numbers[i];
-            }
-        } else {
-            if(numbers[i] == numbers[i + forward]) {
-                sum += numbers[i];
-            }
-        }
-    }
-    return sum;
+function part_2(): number {
+    return arr.reduce((acc, num, i) => num === arr[(i + arr.length / 2) % arr.length] ? acc + num : acc, 0);
 }
