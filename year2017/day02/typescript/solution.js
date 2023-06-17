9ddb3498ea3e394e85f579ff87174d47a5781fb5
marcel-noehre <marcelnoehre@web.de>
2023-06-17 10:34:22 +0200

    cleanup 2017/02

--- year2017/day02/typescript/solution.js
+++ year2017/day02/typescript/solution.js
@@ -3,29 +3,17 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().trim().split('\r\n').map(function (row) { return row.split('\t').map(function (number) { return parseInt(number, 10); }); });
-console.log(part_1(arr));
-console.log(part_2(arr));
-function part_1(spreadsheet) {
-    var checksum = 0;
-    for (var i = 0; i < spreadsheet.length; i++) {
-        checksum += Math.max.apply(Math, spreadsheet[i]) - Math.min.apply(Math, spreadsheet[i]);
-    }
-    return checksum;
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return arr.reduce(function (acc, row) { return acc + Math.max.apply(Math, row) - Math.min.apply(Math, row); }, 0);
 }
-function part_2(spreadsheet) {
-    var checksum = 0;
-    for (var i = 0; i < spreadsheet.length; i++) {
-        for (var j = 0; j < spreadsheet[i].length - 1; j++) {
-            for (var x = j + 1; x < spreadsheet[i].length; x++) {
-                var max = Math.max(spreadsheet[i][j], spreadsheet[i][x]);
-                var min = Math.min(spreadsheet[i][j], spreadsheet[i][x]);
-                if (max % min === 0) {
-                    checksum += max / min;
-                    j = spreadsheet[i].length;
-                    x = spreadsheet[i].length;
-                }
-            }
-        }
-    }
-    return checksum;
+function part_2() {
+    return arr.reduce(function (acc, row) {
+        var even = row.reduce(function (pair, num, index) {
+            var div = row.find(function (n, i) { return i !== index && n % num === 0; });
+            return div ? [div, num] : pair;
+        }, []);
+        return acc + even[0] / even[1];
+    }, 0);
 }
--- year2017/day02/typescript/solution.ts
+++ year2017/day02/typescript/solution.ts
@@ -2,31 +2,19 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr: number[][] = file.toString().trim().split('\r\n').map((row) => row.split('\t').map((number) => parseInt(number, 10)));
-console.log(part_1(arr));
-console.log(part_2(arr));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(spreadsheet: number[][]): number {
-    let checksum: number = 0;
-    for(let i = 0; i < spreadsheet.length; i++) {
-        checksum += Math.max(...spreadsheet[i]) - Math.min(...spreadsheet[i]);
-    }
-    return checksum;
+function part_1(): number {
+    return arr.reduce((acc, row) => acc + Math.max(...row) - Math.min(...row), 0);
 }
 
-function part_2(spreadsheet: number[][]): number {
-    let checksum: number = 0;
-    for(let i = 0; i < spreadsheet.length; i++) {
-        for(let j = 0; j < spreadsheet[i].length-1; j++) {
-            for(let x = j+1; x < spreadsheet[i].length; x++) {
-                let max: number = Math.max(spreadsheet[i][j], spreadsheet[i][x]);
-                let min: number = Math.min(spreadsheet[i][j], spreadsheet[i][x]);
-                if(max % min === 0) {
-                    checksum += max/min;
-                    j = spreadsheet[i].length;
-                    x = spreadsheet[i].length;
-                }
-            }
-        }
-    }
-    return checksum;
+function part_2(): number {
+    return arr.reduce((acc, row) => {
+        const even = row.reduce((pair, num, index) => {
+            const div = row.find((n, i) => i !== index && n % num === 0);
+            return div ? [div, num] : pair;
+        }, []);
+        return acc + even[0] / even[1];
+    }, 0);
 }
