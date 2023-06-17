c31179643f372f2e774f56bf091fe3208a0ba884
marcel-noehre <marcelnoehre@web.de>
2023-06-17 12:20:51 +0200

    cleanup 2021/01

--- year2021/day01/typescript/solution.js
+++ year2021/day01/typescript/solution.js
@@ -3,25 +3,11 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().trim().split('\n').map(function (num) { return parseInt(num, 10); });
-console.log(part_1(arr));
-console.log(part_2(arr));
-function part_1(arr) {
-    var counter = 0;
-    for (var i = 0; i < arr.length - 1; i++) {
-        if (arr[i + 1] > arr[i]) {
-            counter++;
-        }
-    }
-    return counter;
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return arr.reduce(function (acc, num, i) { return (i > 0 && num > arr[i - 1] ? acc + 1 : acc); }, 0);
 }
-function part_2(arr) {
-    var counter = 0;
-    for (var i = 0; i < arr.length - 3; i++) {
-        var firstGroup = arr[i] + arr[i + 1] + arr[i + 2];
-        var secondGroup = arr[i + 1] + arr[i + 2] + arr[i + 3];
-        if (secondGroup > firstGroup) {
-            counter++;
-        }
-    }
-    return counter;
+function part_2() {
+    return arr.reduce(function (acc, _, i) { return i < arr.length - 3 && arr[i + 1] + arr[i + 2] + arr[i + 3] > arr[i] + arr[i + 1] + arr[i + 2] ? acc + 1 : acc; }, 0);
 }
--- year2021/day01/typescript/solution.ts
+++ year2021/day01/typescript/solution.ts
@@ -2,27 +2,13 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr: number[] = file.toString().trim().split('\n').map((num) => parseInt(num, 10));
-console.log(part_1(arr));
-console.log(part_2(arr));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(arr: number[]):number {
-    let counter: number = 0;
-    for(let i = 0; i < arr.length-1; i++) {
-        if(arr[i+1] > arr[i]) {
-            counter++;
-        }
-    }
-    return counter;
+function part_1():number {
+    return arr.reduce((acc, num, i) => (i > 0 && num > arr[i - 1] ? acc + 1 : acc), 0);
 }
 
-function part_2(arr: number[]):number {
-    let counter: number = 0;
-    for(let i = 0; i < arr.length-3; i++) {
-        let firstGroup: number = arr[i]+arr[i+1]+arr[i+2];
-        let secondGroup: number = arr[i+1]+arr[i+2]+arr[i+3];
-        if(secondGroup>firstGroup) {
-            counter++;
-        }
-    }
-    return counter;
+function part_2():number {
+    return arr.reduce((acc, _, i) => i < arr.length - 3 && arr[i + 1] + arr[i + 2] + arr[i + 3] > arr[i] + arr[i + 1] + arr[i + 2] ? acc + 1 : acc, 0);
 }
