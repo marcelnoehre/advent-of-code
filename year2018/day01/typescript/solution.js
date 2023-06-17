4c78e9255559bae3735c3a0ca46d64649b48ee80
marcel-noehre <marcelnoehre@web.de>
2023-06-17 10:57:10 +0200

    cleanup 2018/01

--- year2018/day01/typescript/solution.js
+++ year2018/day01/typescript/solution.js
@@ -3,27 +3,20 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().trim().split('\n').map(function (num) { return parseInt(num, 10); });
-console.log(part_1(arr));
-console.log(part_2(arr));
-function part_1(numbers) {
-    var frequency = 0;
-    for (var i = 0; i < numbers.length; i++) {
-        frequency += numbers[i];
-    }
-    return frequency;
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    return arr.reduce(function (acc, num) { return acc + num; }, 0);
 }
-function part_2(numbers) {
-    var frequencies = [0];
-    var frequency = 0;
-    do {
-        for (var i = 0; i < numbers.length; i++) {
-            frequency += numbers[i];
-            if (frequencies.includes(frequency)) {
+function part_2() {
+    var _a = [new Set(), 0], frequencies = _a[0], frequency = _a[1];
+    while (true) {
+        for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+            var number = arr_1[_i];
+            if (frequencies.has(frequency += number)) {
                 return frequency;
             }
-            else {
-                frequencies.push(frequency);
-            }
+            frequencies.add(frequency);
         }
-    } while (true);
+    }
 }
--- year2018/day01/typescript/solution.ts
+++ year2018/day01/typescript/solution.ts
@@ -2,28 +2,22 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr:number[] = file.toString().trim().split('\n').map((num) => parseInt(num, 10));
-console.log(part_1(arr));
-console.log(part_2(arr));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(numbers: number[]): number {
-    let frequency: number = 0;
-    for(let i = 0; i < numbers.length; i++) {
-        frequency += numbers[i];
-    }
-    return frequency;
+function part_1(): number {
+    return arr.reduce((acc, num) => acc + num, 0);
+
 }
 
-function part_2(numbers: number[]):number {
-    let frequencies: number[] = [0];
-    let frequency: number = 0;
-    do {
-        for(let i = 0; i < numbers.length; i++) {
-            frequency += numbers[i];
-            if(frequencies.includes(frequency)) {
+function part_2(): number {
+    let [frequencies, frequency] = [new Set<number>(), 0];
+    while (true) {
+        for (const number of arr) {
+            if (frequencies.has(frequency += number)) {
                 return frequency;
-            } else {
-                frequencies.push(frequency);
-            }
+            } 
+            frequencies.add(frequency);
         }
-    } while(true);
+    }
 }
