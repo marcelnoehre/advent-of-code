d8daffa649c91afac141421ac90ea33aa87aa79c
marcel-noehre <marcelnoehre@web.de>
2023-06-18 22:21:07 +0200

    cleanup 2021/03

--- year2021/day03/typescript/solution.js
+++ year2021/day03/typescript/solution.js
@@ -3,51 +3,37 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().trim().split('\n').map(function (line) { return line.replace('\r', ''); });
-console.log(part_1(arr));
-console.log(part_2(arr));
-function part_1(arr) {
-    var counter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    var half = arr.length / 2;
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr[j].length; j++) {
-            if (arr[i][j] == '1') {
-                counter[j]++;
-            }
-        }
-    }
-    var gamma = '';
-    var epsilon = '';
-    for (var i = 0; i < counter.length; i++) {
-        gamma += counter[i] > half ? '1' : '0';
-        epsilon += counter[i] > half ? '0' : '1';
-    }
-    return parseInt(gamma, 2) * parseInt(epsilon, 2);
-}
-function part_2(arr) {
-    var oxygen = arr;
-    var co2 = arr;
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    var _a = ['', ''], gamma = _a[0], epsilon = _a[1];
     var _loop_1 = function (i) {
-        if (oxygen.length > 1) {
-            var counter_1 = 0;
-            for (var j = 0; j < oxygen.length; j++) {
-                if (oxygen[j][i] == '1') {
-                    counter_1++;
-                }
-            }
-            oxygen = oxygen.filter(function (binary) { return binary[i] == (counter_1 >= oxygen.length / 2 ? '1' : '0'); });
-        }
-        if (co2.length > 1) {
-            var counter_2 = 0;
-            for (var j = 0; j < co2.length; j++) {
-                if (co2[j][i] == '1') {
-                    counter_2++;
-                }
-            }
-            co2 = co2.filter(function (binary) { return binary[i] != (counter_2 < co2.length / 2 ? '0' : '1'); });
-        }
+        var counter = 0;
+        arr.forEach(function (binary) { return counter += binary[i] === '0' ? 1 : 0; });
+        gamma += counter > arr.length / 2 ? '0' : '1';
+        epsilon += counter > arr.length / 2 ? '1' : '0';
     };
     for (var i = 0; i < arr[0].length; i++) {
         _loop_1(i);
+    }
+    return parseInt(gamma, 2) * parseInt(epsilon, 2);
+}
+function part_2() {
+    var _a = [arr, arr], oxygen = _a[0], co2 = _a[1];
+    var _loop_2 = function (i) {
+        if (oxygen.length > 1) {
+            var counter_1 = 0;
+            oxygen.forEach(function (binary) { return counter_1 += binary[i] === '1' ? 1 : 0; });
+            oxygen = oxygen.filter(function (binary) { return binary[i] === (counter_1 >= oxygen.length / 2 ? '1' : '0'); });
+        }
+        if (co2.length > 1) {
+            var counter_2 = 0;
+            co2.forEach(function (binary) { return counter_2 += binary[i] === '1' ? 1 : 0; });
+            co2 = co2.filter(function (binary) { return binary[i] !== (counter_2 < co2.length / 2 ? '0' : '1'); });
+        }
+    };
+    for (var i = 0; i < arr[0].length; i++) {
+        _loop_2(i);
     }
     return parseInt(oxygen[0], 2) * parseInt(co2[0], 2);
 }
--- year2021/day03/typescript/solution.ts
+++ year2021/day03/typescript/solution.ts
@@ -2,49 +2,32 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr:string[] = file.toString().trim().split('\n').map((line) => line.replace('\r', ''));
-console.log(part_1(arr));
-console.log(part_2(arr));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(arr: string[]):number {
-    let counter: number[] = [0,0,0,0,0,0,0,0,0,0,0,0];
-    let half: number = arr.length/2;
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr[j].length; j++) {
-            if(arr[i][j] == '1') {
-                counter[j]++;
-            }
-        }
-    }
-    let gamma: string = '';
-    let epsilon: string = '';
-    for(let i = 0; i < counter.length; i++) {
-        gamma += counter[i] > half ? '1' : '0';
-        epsilon += counter[i] > half ? '0' : '1'; 
+function part_1(): number {
+    let [gamma, epsilon] = ['', ''];
+    for(let i = 0; i < arr[0].length; i++) {
+        let counter: number = 0;
+        arr.forEach((binary) => counter += binary[i] === '0' ? 1 : 0);
+        gamma += counter > arr.length / 2 ? '0' : '1';
+        epsilon += counter > arr.length / 2 ? '1' : '0';
     }
     return parseInt(gamma, 2) * parseInt(epsilon, 2);
 }
 
-function part_2(arr: string[]):number {
-    let oxygen: string[] = arr;
-    let co2: string[] = arr;
+function part_2(): number {
+    let [oxygen, co2] = [arr, arr];
     for(let i = 0; i < arr[0].length; i++) {
         if(oxygen.length > 1) {
             let counter: number = 0;
-            for(let j = 0; j < oxygen.length; j++) {
-                if(oxygen[j][i] == '1') {
-                    counter++;
-                }
-            }
-            oxygen = oxygen.filter((binary) => binary[i] == (counter >= oxygen.length/2? '1' : '0'));
+            oxygen.forEach((binary) => counter += binary[i] === '1' ? 1 : 0); 
+            oxygen = oxygen.filter((binary) => binary[i] === (counter >= oxygen.length / 2 ? '1' : '0'));
         }
         if(co2.length > 1) {
             let counter: number = 0;
-            for(let j = 0; j < co2.length; j++) {
-                if(co2[j][i] == '1') {
-                    counter++;
-                }
-            }
-            co2 = co2.filter((binary) => binary[i] != (counter < co2.length/2? '0' : '1'));
+            co2.forEach((binary) => counter += binary[i] === '1' ? 1 : 0);
+            co2 = co2.filter((binary) => binary[i] !== (counter < co2.length / 2 ? '0' : '1'));
         }
     }
     return parseInt(oxygen[0], 2) * parseInt(co2[0], 2);
