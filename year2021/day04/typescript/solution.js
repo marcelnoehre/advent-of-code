a66a49ae5d49301a6be0428f6ece79a326fdc114
marcel-noehre <marcelnoehre@web.de>
2023-06-18 22:47:56 +0200

    cleanup 2021/04

--- year2021/day04/typescript/solution.js
+++ year2021/day04/typescript/solution.js
@@ -3,119 +3,49 @@ exports.__esModule = true;
 var fs_1 = require("fs");
 var file = (0, fs_1.readFileSync)('../puzzle.txt', 'utf-8');
 var arr = file.toString().trim().split('\r\n\r\n');
-var numbers = arr[0].trim().split(',').map(function (num) { return parseInt(num, 10); });
+var numbers = arr[0].trim().split(",").map(Number);
 var boards = arr.slice(1, arr.length).map(function (board) { return board.split('\r\n').map(function (row) { return row.trim().split(/\s+/).map(function (number) { return parseInt(number, 10); }); }); });
-console.log(part_1(numbers, boards));
-console.log(part_2(numbers, boards));
-function part_1(numbers, boards) {
-    var drawn = [];
-    var latest;
-    for (var i = 0; i < numbers.length; i++) {
-        drawn.push(numbers[i]);
-        latest = numbers[i];
-        for (var x = 0; x < boards.length; x++) {
-            if (drawn.length >= 5) {
-                var bingo = false;
-                for (var y = 0; y < 5; y++) {
-                    var horizontal = true;
-                    for (var z = 0; z < 5; z++) {
-                        if (!drawn.includes(boards[x][y][z])) {
-                            horizontal = false;
-                            z = 5;
-                        }
-                    }
-                    if (horizontal) {
-                        bingo = true;
-                        y = 5;
-                    }
-                }
-                if (!bingo) {
-                    for (var z = 0; z < 5; z++) {
-                        var vertical = true;
-                        for (var y = 0; y < 5; y++) {
-                            if (!drawn.includes(boards[x][y][z])) {
-                                vertical = false;
-                                y = 5;
-                            }
-                        }
-                        if (vertical) {
-                            bingo = true;
-                            z = 5;
-                        }
-                    }
-                }
-                if (bingo) {
-                    var sum = 0;
-                    for (var y = 0; y < 5; y++) {
-                        for (var z = 0; z < 5; z++) {
-                            if (!drawn.includes(boards[x][y][z])) {
-                                sum += boards[x][y][z];
-                            }
-                        }
-                    }
-                    return sum * latest;
-                }
+console.log(part_1());
+console.log(part_2());
+function part_1() {
+    var marked = new Set();
+    for (var _i = 0, numbers_1 = numbers; _i < numbers_1.length; _i++) {
+        var num = numbers_1[_i];
+        marked.add(num);
+        var _loop_1 = function (board) {
+            if (board.some(function (row) { return row.every(function (x) { return marked.has(x); }); }) || board.some(function (_, columnIndex) { return board.every(function (row) { return marked.has(row[columnIndex]); }); })) {
+                return { value: num * board.flat().reduce(function (total, x) { return (marked.has(x) ? total : total + x); }, 0) };
             }
+        };
+        for (var _a = 0, boards_1 = boards; _a < boards_1.length; _a++) {
+            var board = boards_1[_a];
+            var state_1 = _loop_1(board);
+            if (typeof state_1 === "object")
+                return state_1.value;
         }
     }
 }
-function part_2(numbers, boards) {
-    var drawn = [];
-    var done = [];
-    var latest;
-    for (var i = 0; i < numbers.length; i++) {
-        drawn.push(numbers[i]);
-        latest = numbers[i];
-        for (var x = 0; x < boards.length; x++) {
-            if (!done.includes(x)) {
-                if (drawn.length >= 5) {
-                    var bingo = false;
-                    for (var y = 0; y < 5; y++) {
-                        var horizontal = true;
-                        for (var z = 0; z < 5; z++) {
-                            if (!drawn.includes(boards[x][y][z])) {
-                                horizontal = false;
-                                z = 5;
-                            }
-                        }
-                        if (horizontal) {
-                            bingo = true;
-                            y = 5;
-                        }
-                    }
-                    if (!bingo) {
-                        for (var z = 0; z < 5; z++) {
-                            var vertical = true;
-                            for (var y = 0; y < 5; y++) {
-                                if (!drawn.includes(boards[x][y][z])) {
-                                    vertical = false;
-                                    y = 5;
-                                }
-                            }
-                            if (vertical) {
-                                bingo = true;
-                                z = 5;
-                            }
-                        }
-                    }
-                    if (bingo) {
-                        if (done.length == boards.length - 1) {
-                            var sum = 0;
-                            for (var y = 0; y < 5; y++) {
-                                for (var z = 0; z < 5; z++) {
-                                    if (!drawn.includes(boards[x][y][z])) {
-                                        sum += boards[x][y][z];
-                                    }
-                                }
-                            }
-                            return sum * latest;
-                        }
-                        else {
-                            done.push(x);
-                        }
-                    }
+function part_2() {
+    var _a = [new Set(), new Set()], won = _a[0], marked = _a[1];
+    for (var _i = 0, numbers_2 = numbers; _i < numbers_2.length; _i++) {
+        var num = numbers_2[_i];
+        marked.add(num);
+        var _loop_2 = function (i) {
+            if (won.has(i)) {
+                return "continue";
+            }
+            var b = boards[i];
+            if (b.some(function (row) { return row.every(function (x) { return marked.has(x); }); }) || b.some(function (_, columnIndex) { return b.every(function (row) { return marked.has(row[columnIndex]); }); })) {
+                won.add(i);
+                if (won.size === boards.length) {
+                    return { value: num * b.flat().reduce(function (total, x) { return marked.has(x) ? total : total + x; }, 0) };
                 }
             }
+        };
+        for (var i = 0; i < boards.length; i++) {
+            var state_2 = _loop_2(i);
+            if (typeof state_2 === "object")
+                return state_2.value;
         }
     }
 }
--- year2021/day04/typescript/solution.ts
+++ year2021/day04/typescript/solution.ts
@@ -2,119 +2,37 @@ import { readFileSync } from 'fs';
 
 const file:any = readFileSync('../puzzle.txt', 'utf-8');
 const arr:string[] = file.toString().trim().split('\r\n\r\n');
-const numbers: number[] = arr[0].trim().split(',').map((num) => parseInt(num, 10));
+const numbers: number[] = arr[0].trim().split(",").map(Number);
 const boards: number[][][] = arr.slice(1, arr.length).map((board) => board.split('\r\n').map((row) => row.trim().split(/\s+/).map((number) => parseInt(number, 10))))
-console.log(part_1(numbers, boards));
-console.log(part_2(numbers, boards));
+console.log(part_1());
+console.log(part_2());
 
-function part_1(numbers: number[], boards: number[][][]):number {
-    let drawn: number[] = [];
-    let latest: number;
-    for(let i = 0; i < numbers.length; i++) {
-        drawn.push(numbers[i]);
-        latest = numbers[i];
-        for(let x = 0; x < boards.length; x++) {
-            if(drawn.length >= 5) {
-                let bingo: boolean = false;
-                for(let y = 0; y < 5; y++) {
-                    let horizontal: boolean = true;
-                    for(let z = 0; z < 5; z++) {
-                        if(!drawn.includes(boards[x][y][z])) {
-                            horizontal = false;
-                            z = 5;
-                        }
-                    }
-                    if(horizontal) {
-                        bingo = true;
-                        y = 5;
-                    }
-                }
-                if(!bingo) {
-                    for(let z = 0; z < 5; z++) {
-                        let vertical: boolean = true;
-                        for(let y = 0; y < 5; y++) {
-                            if(!drawn.includes(boards[x][y][z])) {
-                                vertical = false;
-                                y = 5;
-                            }
-                        }
-                        if(vertical) {
-                            bingo = true;
-                            z = 5;
-                        }
-                    }
-                }
-                if(bingo) {
-                    let sum = 0; 
-                    for(let y = 0; y < 5; y++) {
-                        for(let z = 0; z < 5; z++) {
-                            if(!drawn.includes(boards[x][y][z])) {
-                                sum += boards[x][y][z];
-                            }
-                        }
-                    }
-                    return sum * latest;
-                }
+function part_1(): number {
+    const marked: Set<number> = new Set<number>();
+    for(const num of numbers) {
+        marked.add(num);
+        for(const board of boards) {
+            if (board.some((row) => row.every((x) => marked.has(x))) || board.some((_, columnIndex) => board.every((row) => marked.has(row[columnIndex])))) {
+                return num * board.flat().reduce((total, x) => (marked.has(x) ? total : total + x), 0);
             }
         }
     }
 }
 
-function part_2(numbers: number[], boards: number[][][]): number {
-    let drawn: number[] = [];
-    let done: number[] = [];
-    let latest: number;
-    for(let i = 0; i < numbers.length; i++) {
-        drawn.push(numbers[i]);
-        latest = numbers[i];
-        for(let x = 0; x < boards.length; x++) {
-            if(!done.includes(x)) {
-                if(drawn.length >= 5) {
-                    let bingo: boolean = false;
-                    for(let y = 0; y < 5; y++) {
-                        let horizontal: boolean = true;
-                        for(let z = 0; z < 5; z++) {
-                            if(!drawn.includes(boards[x][y][z])) {
-                                horizontal = false;
-                                z = 5;
-                            }
-                        }
-                        if(horizontal) {
-                            bingo = true;
-                            y = 5;
-                        }
-                    }
-                    if(!bingo) {
-                        for(let z = 0; z < 5; z++) {
-                            let vertical: boolean = true;
-                            for(let y = 0; y < 5; y++) {
-                                if(!drawn.includes(boards[x][y][z])) {
-                                    vertical = false;
-                                    y = 5;
-                                }
-                            }
-                            if(vertical) {
-                                bingo = true;
-                                z = 5;
-                            }
-                        }
-                    }
-                    if(bingo) {
-                        if(done.length == boards.length-1) {
-                            let sum = 0; 
-                            for(let y = 0; y < 5; y++) {
-                                for(let z = 0; z < 5; z++) {
-                                    if(!drawn.includes(boards[x][y][z])) {
-                                        sum += boards[x][y][z];
-                                    }
-                                }
-                            }
-                            return sum * latest;       
-                        } else {
-                            done.push(x);
-                        }
-                    }
-                } 
+function part_2(): number {
+    const [won, marked] = [new Set<number>(), new Set<number>()];
+    for (const num of numbers) {
+        marked.add(num);
+        for (let i = 0; i < boards.length; i++) {
+            if (won.has(i)) {
+                continue;
+            }
+            const b = boards[i];
+            if (b.some((row) => row.every((x) => marked.has(x))) || b.some((_, columnIndex) => b.every((row) => marked.has(row[columnIndex])))) {
+                won.add(i);
+                if (won.size === boards.length) {
+                    return num * b.flat().reduce((total, x) => marked.has(x) ? total : total + x, 0);
+                }
             }
         }
     }
