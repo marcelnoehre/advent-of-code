2a412040636bfa762ae7b2b7cd19eb361eb91691
marcelnoehre <marcelnoehre@web.de>
2023-10-14 13:24:06 +0200

    typescript + java template

---
+++ templates/java/Solution.java
@@ -0,0 +1,24 @@
+package yearYYYY.dayDD.java;
+
+import java.nio.file.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) { 
+        try {
+            Files.readAllLines(Paths.get("../puzzle.txt"));
+            System.out.println(part1());
+            System.out.println(part2());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+	private static Integer part1() {
+        return null;
+    }
+    
+    private static Integer part2() {
+        return null;
+    }
+}
---
+++ templates/typescript/solution.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from 'fs';
+
+const file:any = readFileSync('../puzzle.txt', 'utf-8');
+const arr:string[][] = file.toString();
+console.log(part_1());
+console.log(part_2());
+
+function part_1(): number {
+    return null;
+}
+
+function part_2(): number {
+    return null;
+}
