518503ccd9f5b39fb0429d06c6e0f2b227b027e9
marcelnoehre <marcelnoehre@web.de>
2023-10-24 21:22:02 +0200

    typescript solution 2022/20

--- README.md
+++ README.md
@@ -253,7 +253,7 @@
 [<img src="https://badgen.net/badge/17/★★/yellow">](https://github.com/marcelnoehre/advent-of-code/tree/master/year2022/day17/typescript)
 [<img src="https://badgen.net/badge/18/★★/yellow">](https://github.com/marcelnoehre/advent-of-code/tree/master/year2022/day18/typescript)
 [<img src="https://badgen.net/badge/19/★★/yellow">](https://github.com/marcelnoehre/advent-of-code/tree/master/year2022/day19/typescript)
-![20](https://badgen.net/badge/20/☆☆/gray)
+[<img src="https://badgen.net/badge/20/★★/yellow">](https://github.com/marcelnoehre/advent-of-code/tree/master/year2022/day20/typescript)
 
 ![21](https://badgen.net/badge/21/☆☆/gray)
 ![22](https://badgen.net/badge/22/☆☆/gray)
---
+++ year2022/day20/typescript/solution.ts
@@ -0,0 +1,34 @@
+import { readFileSync } from 'fs';
+
+const file:any = readFileSync('../puzzle.txt', 'utf-8');
+const arr:number[] = file.toString().split('\n').map(Number);
+console.log(part_1());
+console.log(part_2());
+
+function part_1(): number {
+    return iterate(1);
+}
+
+function part_2(): number {
+    return iterate(2);
+}
+
+function iterate(part: number): number {
+    const numbers = part === 1 ? arr : arr.map(x => x * 811589153);
+    let [q, c] = [numbers.slice().map((v, i) => [i, v]), 0];
+    for (let m = 0; m < (part === 2 ? 10 : 1); m++) {
+        for (let i = 0; i < q.length; i++) {
+            for (let j = 0; j < q.length; j++) {
+                if (q[j][0] === i) break;
+            }
+            while (q[0][0] !== i) q.push(q.shift()!);
+            const val = q.shift()!;
+            let toPop = val[1] % q.length;
+            if (toPop < 0) toPop += q.length;
+            for (let k = 0; k < toPop; k++) q.push(q.shift()!);
+            q.push(val);
+        }
+    }
+    for (c; c < q.length; c++) if (q[c][1] === 0) break;
+    return q[(c + 1000) % q.length][1] + q[(c + 2000) % q.length][1] + q[(c + 3000) % q.length][1];
+}
