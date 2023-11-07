efe05e1b059c02f84874e61da2b951c7de12f57f
marcelnoehre <marcelnoehre@web.de>
2023-11-07 01:18:30 +0100

    python solution 2021/03

--- README.md
+++ README.md
@@ -200,6 +200,7 @@
    <a href="https://github.com/marcelnoehre/advent-of-code/tree/master/year2021/day03">
     03
     <img height="20" src="assets/typescript.svg"/>
+    <img height="20" src="assets/python.svg"/>
    </a>
   </td>
   <td>
---
+++ year2021/day03/python/solution.py
@@ -0,0 +1,56 @@
+def main():
+    with open('../puzzle.txt') as file:
+        input = [row.strip() for row in file]
+    
+    print(part1(input))
+    print(part2(input))
+
+
+def part1(input):
+    g, e = '', ''
+
+    for i in range(len(input[0])):
+        dpp = [binary[i] for binary in input]
+
+        if dpp.count('0') > len(input) / 2:
+            g += '0'
+            e += '1'
+        else:
+            g += '1'
+            e += '0'
+
+    return int(g, 2) * int(e, 2)
+
+
+def part2(input):
+    o, c = input, input
+
+    for i in range(len(o[0])):
+        if len(o) == 1:
+            break
+
+        dpp = [binary[i] for binary in o]
+        if dpp.count('1') >= len(o) / 2:
+            cbit = '1'  
+        else: 
+            cbit = '0'
+
+        o = [binary for binary in o if binary[i] == cbit]
+
+    for i in range(len(c[0])):
+        if len(c) == 1:
+            break
+
+        dpp = [binary[i] for binary in c]
+        if dpp.count('1') >= len(c) / 2:
+            lcbit = '0'  
+        else: 
+            lcbit = '1'
+
+        c = [binary for binary in c if binary[i] == lcbit]
+
+    return int(o[0], 2) * int(c[0], 2)
+
+
+if __name__ == '__main__':
+    main()
