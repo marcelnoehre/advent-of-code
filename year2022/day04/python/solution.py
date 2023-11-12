caf0e349c95733f130e91d89c6ae8fb21908badd
marcelnoehre <marcelnoehre@web.de>
2023-11-12 23:21:30 +0100

    python solution 2022/04

---
+++ year2022/day04/python/solution.py
@@ -0,0 +1,26 @@
+def main():
+    with open('../puzzle.txt') as file:
+        input = [list(map(lambda tupel: list(map(int, tupel.split('-'))), group.split(','))) for group in file.read().split('\n')]
+    
+    print(part1(input))
+    print(part2(input))
+
+
+def part1(input):
+    return len(list(filter(lambda pair: overlaps(pair[0], pair[1]), input)))
+
+
+def part2(input):
+    return len(list(filter(lambda pair: contains(pair[0], pair[1]), input)))
+
+
+def overlaps(pair1, pair2):
+    return (pair1[0] <= pair2[0] and pair1[1] >= pair2[1]) or (pair2[0] <= pair1[0] and pair2[1] >= pair1[1])
+
+
+def contains(pair1, pair2):
+    return ((pair1[0] >= pair2[0] and pair1[0] <= pair2[1]) or (pair2[0] >= pair1[0] and pair2[0] <= pair1[1]) or (pair1[1] <= pair2[1] and pair1[1] >= pair2[0]) or (pair2[1] <= pair1[1] and pair2[1] >= pair1[0]))
+
+
+if __name__ == '__main__':
+    main()
