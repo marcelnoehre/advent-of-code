1482afc67efcedbb60cf912fec09d34943eeaf66
marcelnoehre <marcelnoehre@web.de>
2023-11-15 22:10:43 +0100

    java solution 2021/02

--- README.md
+++ README.md
@@ -232,6 +232,7 @@ test.bat
    <a href="https://github.com/marcelnoehre/advent-of-code/tree/master/year2021/day02">
     02
     <img height="20" src="assets/typescript.svg"/>
+    <img height="20" src="assets/java.svg"/>
     <img height="20" src="assets/python.svg"/>
    </a>
   </td>
---
+++ year2021/day02/java/Solution.java
@@ -0,0 +1,64 @@
+import java.nio.file.*;
+import java.util.*;
+
+public class Solution {
+    private static String[] DIRECTIONS;
+    private static int[] RANGES;
+
+    public static void main(String[] args) { 
+        try {
+            setup(Files.readAllLines(Paths.get("../puzzle.txt")));
+            System.out.println(part1());
+            System.out.println(part2());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+	public static Integer part1() {
+        int height = 0;
+        int depth = 0;
+
+        for(int i = 0; i < RANGES.length; i++) {
+            if (DIRECTIONS[i].equals("forward")) {
+                height += RANGES[i];
+
+            } else if (DIRECTIONS[i].equals("up")) {
+                depth -= RANGES[i];
+
+            } else if (DIRECTIONS[i].equals("down")) {
+                depth += RANGES[i];
+
+            }
+        }
+
+        return height * depth;
+    }
+    
+    public static Integer part2() {
+        int aim = 0;
+        int height = 0;
+        int depth = 0;
+
+        for (int i = 0; i < RANGES.length; i++) {
+            if (DIRECTIONS[i].equals("forward")) {
+                height += RANGES[i];
+                depth += RANGES[i] * (aim * -1);
+
+            } else if (DIRECTIONS[i].equals("up")) {
+                aim += RANGES[i];
+
+            } else if (DIRECTIONS[i].equals("down")) {
+                aim -= RANGES[i];
+
+            }
+        }
+
+        return height * depth;
+    }
+
+    public static void setup(List<String> input) {
+        DIRECTIONS = input.stream().map(line -> line.split(" ")[0]).toArray(String[]::new);
+        RANGES = input.stream().map(line -> Integer.parseInt(line.split(" ")[1])).mapToInt(Integer::intValue).toArray();
+    }
+}
