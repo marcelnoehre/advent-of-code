97cd36158617d27316d64a54775536856ad085d5
marcelnoehre <marcelnoehre@web.de>
2023-11-16 15:55:15 +0100

    java solution 2015/04

--- README.md
+++ README.md
@@ -1140,6 +1140,7 @@ test.bat
    <a href="https://github.com/marcelnoehre/advent-of-code/tree/master/year2015/day04">
     04
     <img height="20" src="assets/typescript.svg"/>
+    <img height="20" src="assets/java.svg"/>
     <img height="20" src="assets/python.svg"/>
    </a>
   </td>
---
+++ year2015/day04/java/Solution.java
@@ -0,0 +1,49 @@
+import java.nio.file.*;
+import java.util.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Solution {
+    private static String HASH;
+
+    public static void main(String[] args) { 
+        try {
+            setup(Files.readString(Paths.get("../puzzle.txt")));
+            System.out.println(part1());
+            System.out.println(part2());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+	public static int part1() {
+        return mine(5);
+    }
+    
+    public static int part2() {
+        return mine(6);
+    }
+
+    private static int mine(int digits) {
+        int counter = 0;
+        try {
+            while (true) {
+                StringBuilder hashStringBuilder = new StringBuilder();
+                for (byte b : MessageDigest.getInstance("MD5").digest((HASH + counter).getBytes())) {
+                    hashStringBuilder.append(String.format("%02x", b));
+                }
+                if (hashStringBuilder.toString().startsWith("0".repeat(digits))) {
+                    break;
+                }
+                counter++;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return counter;
+    }
+
+    public static void setup(String input) {
+        HASH = input;
+    }
+}
