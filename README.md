49d6e7052dfc313d5816d302928b796a9e004a8a
marcelnoehre <marcelnoehre@web.de>
2023-11-18 14:38:43 +0100

    java solution 2018/04

--- README.md
+++ README.md
@@ -720,6 +720,7 @@ test.bat
    <a href="https://github.com/marcelnoehre/advent-of-code/tree/master/year2018/day04">
     04
     <img height="20" src="assets/typescript.svg"/>
+    <img height="20" src="assets/java.svg"/>
    </a>
   </td>
   <td>
---
+++ year2018/day04/java/SolutionSpec.java
@@ -0,0 +1,19 @@
+import java.nio.file.*;
+import java.util.*;
+
+public class SolutionSpec {
+    public static void main(String[] args) { 
+        try {
+            Solution.setup(Files.readString(Paths.get("../example.txt")));
+            test(1, Solution.part1(), 240);
+            test(2, Solution.part2(), 4455);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void test(int part, int solution, int expected) {
+        assert solution == expected : "Part_" + part + ": FAILED - " + "[expected: " + expected + ", solution: " + solution + "]";
+        System.out.println("Part_" + part + ": SUCCESS");
+    }
+}
