5279afbb9047ca6d6efcafac4fdff7afd479a9ab
marcelnoehre <marcelnoehre@web.de>
2023-12-08 12:23:58 +0100

    typescript solution 2023/08

--- README.md
+++ README.md
@@ -89,6 +89,7 @@ test.bat
   <td>
    <a href="https://github.com/marcelnoehre/advent-of-code/tree/master/year2023/day08">
     08
+    <img height="20" src="assets/typescript.svg"/>
    </a>
   </td>
   <td>
---
+++ year2023/day08/typescript/solution.ts
@@ -0,0 +1,25 @@
+import { readFileSync } from 'fs';
+
+const file: any = readFileSync('../' + (process.argv[2] === 'puzzle' ? 'puzzle' : 'example') + '.txt', 'utf-8');
+const input: [any, any] = [file.toString().split('\n\n')[0].split('').map((char) => +(char === 'R')), file.toString().split('\n\n')[1].split('\n').reduce((acc, line) => (([left, right]) => (acc[left] = right.match(/\w+/g), acc)) (line.split(' = ')), {})];
+if(process.argv[2] === 'puzzle') {
+  console.log(part_1());
+  console.log(part_2());
+}
+
+export function part_1(): number {
+  let [curr, i] = ['AAA', 0]; 
+  while (curr !== 'ZZZ') curr = input[1][curr][input[0][i++ % input[0].length]]; 
+  return i;
+}
+
+export function part_2(): number {
+  const kgv = (...nums) => nums.reduce((acc, n) => (acc * n) / ggt(acc, n));
+  const ggt = (...nums) => nums.reduce((acc, n) => (!n ? acc : ggt(n, acc % n)));
+  let [curr, i, loop] = [Object.keys(input[1]).filter((key) => key.endsWith('A')), 0, Object.keys(input[1]).filter((key) => key.endsWith('A')).map(() => null)];
+  while (!loop.every(Boolean)) {
+    curr = curr.map((key, j) => loop[j] ? key : key.endsWith('Z') ? (loop[j] = i, key) : input[1][key][input[0][i % input[0].length]]);
+    i++;
+  }
+  return kgv(...loop);
+}
