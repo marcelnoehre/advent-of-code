f1f5e339d3a6d22085f6ebfa91489e75077f6bf5
marcelnoehre <marcelnoehre@web.de>
2024-12-19 22:30:48 +0100

    typescript solution 2024/19

---
+++ year2024/day19/typescript/solution-2024-19.spec.ts
@@ -0,0 +1,11 @@
+import { part_1, part_2 } from './solution';
+
+describe('Advent of Code 2024, Day 19', () => {
+  it('Part One', () => {
+    expect(part_1()).toBe(6);
+  });
+
+  it('Part Two', () => {
+    expect(part_2()).toBe(16);
+  });
+});
---
+++ year2024/day19/typescript/solution.ts
@@ -0,0 +1,26 @@
+import { readFileSync } from 'fs';
+
+const file: any = readFileSync('../' + (process.argv[2] === 'puzzle' ? 'puzzle' : 'example') + '.txt', 'utf-8');
+const input = file.toString().split("\n").map((line, i) => i === 0 ? line.trim().split(", ") : line.trim());
+if(process.argv[2] === 'puzzle') {
+  console.log(part_1());
+  console.log(part_2());
+}
+
+export function part_1(): number {
+  const calc = (d: string, p: string[], m: { [key: string]: boolean | number }): boolean => {
+    if (d in m) return m[d] as boolean;
+    if (!d) return true;
+    for (const pa of p) if (d.startsWith(pa)) if (calc(d.slice(pa.length), p, m)) {m[d] = true; return true;}
+  }
+  return input.slice(2).reduce((acc, e) => acc + (calc(e, input[0], {}) ? 1 : 0), 0);
+}
+
+export function part_2(): number {
+  const calc = (d: string, p: string[], m: { [key: string]: boolean | number }): number => {
+    if (d in m) return m[d] as number;
+    if (!d) return 1;
+    return m[d] = p.reduce((acc, pa) => acc + (d.startsWith(pa) ? calc(d.slice(pa.length), p, m) : 0), 0);
+  }
+  return input.slice(2).reduce((s, e) => s + (calc(e, input[0], {})), 0);
+}
